8f37168e5d2da0e2a7299b38bc3dbd3ed88db2aa
Jay Cee <jakechalmers86@gmail.com>
2025-06-04 15:59:13 +0800

    test increment fucntion (working)

--- Learn-JavaScript/Projects/Passenger-Counter-App/index.js
+++ Learn-JavaScript/Projects/Passenger-Counter-App/index.js
@@ -6,12 +6,15 @@
 
 // intialize the count as 0
 
-// let count = 0;
+let count = 0
 
 // listen for clicks on the increment button
 
 function increment() {
-  console.log("clicked");
+    count = count + 1;
+    console.log(count);
 }
 // increment the count variable when the button is clicked (log it out)
+
+
 // change the count-el in the HTML to reflect the new count
