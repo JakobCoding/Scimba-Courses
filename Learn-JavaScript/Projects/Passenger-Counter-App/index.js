0ace6b0994759bcc434309a48b9610a75c22cd7e
Jay Cee <jakechalmers86@gmail.com>
2025-06-04 16:04:45 +0800

    restructure js comments for readability

--- Learn-JavaScript/Projects/Passenger-Counter-App/index.js
+++ Learn-JavaScript/Projects/Passenger-Counter-App/index.js
@@ -1,20 +1,17 @@
-// document.getElementById("count").innerText = 5
 
-// let count = 5
-// count = count + 1
-// console.log(count)
+
 
 // intialize the count as 0
-
 let count = 0
 
-// listen for clicks on the increment button
+
+// increment the count variable when the button is clicked (log it out)
 
 function increment() {
     count = count + 1;
     console.log(count);
 }
-// increment the count variable when the button is clicked (log it out)
 
-
+// document.getElementById("count").innerText = 5
 // change the count-el in the HTML to reflect the new count
+
