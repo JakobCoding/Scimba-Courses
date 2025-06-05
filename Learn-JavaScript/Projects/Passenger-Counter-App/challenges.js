128d3dfc1c0a9da835daf9c3632d7cf1da42f87a
Jay Cee <jakechalmers86@gmail.com>
2025-06-05 10:04:40 +0800

    Add challanges to challanges.js - link it to HTML

---
+++ Learn-JavaScript/Projects/Passenger-Counter-App/challenges.js
@@ -0,0 +1,21 @@
+// Challenge 1. 
+let username = "per"
+let message = "You have tree new notifications"
+
+console.log(message + ", " + username + "!")
+
+// Create a variable, messageToUser, that contains the message we have logged
+ 
+let messageToUser = message + ", " + username + "!"
+ console.log(messageToUser) // Correct solution 
+
+// Challenge 2: Create two variables, name and greeting. The name variable should store your name, and the greeting should store e.g. "Hi, my name is "
+
+// Create a third variable, myGreeting, that contatenates the two strings
+// Log myGreeting to the console
+
+let firsName = "Jake"
+let greeting = "Hi, my name is "
+let myGreeting = greeting + firsName
+console.log(myGreeting); 
+
