471b032a26d49734db83f5215870202fc40be6c7
Jay Cee <jakechalmers86@gmail.com>
2025-06-07 10:58:43 +0800

    Formatted comments in index.js for readablility

--- Learn-JavaScript/Projects/Passenger-Counter-App/index.js
+++ Learn-JavaScript/Projects/Passenger-Counter-App/index.js
@@ -1,20 +1,23 @@
-// intialize the count as 0
-let count = 0;
+/*
+-intialize the count as 0
+-define countEL variable and assign document.getElementById("count-el") as its value
+*/
 
-// define countEL variable and assign document.getElementById("count-el") as its value
+let count = 0;
 let countEl = document.getElementById("count-el"); // pass in arguments
 
-// Create increment function
+/*
+-Create increment function
+-increment the count variable
+-change the count-el in the HTML to reflect the new count using .innerText
+*/
 
 function increment() {
-  // increment the count variable
   count = count + 1;
-
-  // change the count-el in the HTML to reflect the new count using .innerText
   countEl.innerText = count;
 }
 
-// 1. Create a function, save(), which logs  out the count when it's called
+//- Create a function, save(), which logs  out the count when it's called
 
 function save() {
   console.log(count);
