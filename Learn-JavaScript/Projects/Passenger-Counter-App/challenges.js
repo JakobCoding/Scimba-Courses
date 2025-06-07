fecef9dbb5fc3a76f1958c68ad7bd0ebeac0a3df
Jay Cee <jakechalmers86@gmail.com>
2025-06-07 10:52:22 +0800

    Updated JavaScritpt challanges.js

--- Learn-JavaScript/Projects/Passenger-Counter-App/challenges.js
+++ Learn-JavaScript/Projects/Passenger-Counter-App/challenges.js
@@ -1,31 +1,24 @@
-// Challenge 1. 
-let username = "per"
-let message = "You have tree new notifications"
+// Challenge 1.
+let username = "per";
+let message = "You have tree new notifications";
 
-console.log(message + ", " + username + "!")
+console.log(message + ", " + username + "!");
 
-// Create a variable, messageToUser, that contains the message we have logged
- 
-let messageToUser = message + ", " + username + "!"
- console.log(messageToUser) // Correct solution 
+let messageToUser = message + ", " + username + "!";
+console.log(messageToUser); // Correct solution
 
-// Challenge 2: Create two variables, name and greeting. The name variable should store your name, and the greeting should store e.g. "Hi, my name is "
+let firstName = "Jake";
+let greeting1 = "Hi, my name is ";
+let myGreeting = greeting1 + firstName;
+console.log(myGreeting);
 
-// Create a third variable, myGreeting, that contatenates the two strings
-// Log myGreeting to the console
+let welcomeEl = document.getElementById("welcome-el");
 
-let firstName = "Jake"
-let greeting1 = "Hi, my name is "
-let myGreeting = greeting1 + firstName
-console.log(myGreeting); 
+let name = "Jake campbell Chalmers ";
+let greeting = "Welcome back ";
 
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-let welcomeEl = document.getElementById("welcome-el")
+// Render the welcome message using welcomeEl.innerText to the web page (live server)
+welcomeEl.innerText = greeting + name;
 
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
-let name = "Jake!"
-let greeting = "Hi, Welcome to the page "
-
-// Render the welcome message using welcomeEl.innerText
-welcomeEl.innerText = greeting + name
+// welcomeEl.innerText = welcomeEl.innerText + "👋";
+welcomeEl.innerText += "👋";
--- Learn-JavaScript/Projects/Passenger-Counter-App/index.js
+++ Learn-JavaScript/Projects/Passenger-Counter-App/index.js
@@ -1,5 +1,5 @@
 // intialize the count as 0
-let count = 0
+let count = 0;
 
 // define countEL variable and assign document.getElementById("count-el") as its value
 let countEl = document.getElementById("count-el"); // pass in arguments
@@ -7,16 +7,15 @@ let countEl = document.getElementById("count-el"); // pass in arguments
 // Create increment function
 
 function increment() {
+  // increment the count variable
+  count = count + 1;
 
-// increment the count variable 
-    count = count + 1
-
-// change the count-el in the HTML to reflect the new count using .innerText
-    countEl.innerText = count
+  // change the count-el in the HTML to reflect the new count using .innerText
+  countEl.innerText = count;
 }
 
 // 1. Create a function, save(), which logs  out the count when it's called
 
 function save() {
-    console.log(count);
+  console.log(count);
 }
