43e5df756a4c9a825cbec5ccb6c2bbc4e181eef7
Jay Cee <jakechalmers86@gmail.com>
2025-06-14 19:33:04 +0800

    Using .textcontent to render error message on button click

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -71,10 +71,19 @@ remove1Point();
 remove1Point();
 
 console.log(myPoints);
+
+* RENDERING AN ERROR MESSAGE *
+
+* When the user clicks the purchase button, render out
+* "Something went wrong, please try again" in the paragraph
+* that has the id="error".
+
+let errorParagraph = document.getElementById("error");
+console.log(errorParagraph) 
+
+function errorMessage() {
+    console.log("button clicked");
+    errorParagraph.textContent = "Something went wrong, Please try again";
+}
+
 */
-
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
-
-
