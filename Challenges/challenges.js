0d615202877170af30712872e68e4244be9a1714
Jay Cee <jakechalmers86@gmail.com>
2025-06-14 17:14:31 +0800

    Completed challanges modules

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -1,4 +1,6 @@
-/*// Challenge 1.
+/*
+* CHALLENGE 1 
+
 let username = "per";
 let message = "You have tree new notifications";
 
@@ -17,27 +19,55 @@ let welcomeEl = document.getElementById("welcome-el");
 let name = "Jake campbell Chalmers ";
 let greeting = "Welcome back ";
 
-// Render the welcome message using welcomeEl.innerText to the web page (live server)
+* Render the welcome message using welcomeEl.innerText to the web page (live server)
+
 welcomeEl.innerText = greeting + name;
 
-// welcomeEl.innerText = welcomeEl.innerText + "👋";
+* welcomeEl.innerText = welcomeEl.innerText + "👋";
+
 welcomeEl.innerText += "👋";
+
+
+* Variables Practice // 
+
+ let firstName = "Jake";
+ let lastName = "Matrix Coder";
+ let fullName = firstName + " " +  lastName;
+
+ console.log(fullName);
+
+* CONCATENATE TWO STRINGS IN A FUNCTION // 
+
+ let name = "Linda";
+ let greeting = "Hi there";
+
+ function concatenate() {
+     console.log(greeting + "," + " " + name + "!");
+ }
+ concatenate();
+
+
+
+* INCREMENTING AND DECREMENTING
+* Create two functions, add3Points() and remove1Point(),    and have them
+* add/remove points to/from the myPoints variable
+* Call the functions to that the line below logs out 10
 */
+let myPoints = 3;
 
-// Variables Practice // 
-
-// let firstName = "Jake";
-// let lastName = "Matrix Coder";
-// let fullName = firstName + " " +  lastName;
-
-// console.log(fullName);
-
-// CONCATENATE TWO STRINGS IN A FUNCTION // 
-
-let name = "Linda";
-let greeting = "Hi there";
-
-function concatenate() {
-    console.log(greeting + "," + " " + name + "!");
+function add3Points() {
+    myPoints += 3;
 }
-concatenate();
+
+add3Points();
+add3Points();
+add3Points();
+
+function remove1Point (){
+    myPoints -= 1;
+}
+
+remove1Point();
+remove1Point();
+
+console.log(myPoints);
