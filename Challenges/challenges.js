ad9370d4d2dc0ae05116edda008b127996864e64
Jay Cee <jakechalmers86@gmail.com>
2025-06-14 18:48:53 +0800

    Incrementing and Decrementing functions

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -53,20 +53,20 @@ welcomeEl.innerText += "👋";
 * add/remove points to/from the myPoints variable
 * Call the functions to that the line below logs out 10
 */
+
 let myPoints = 3;
 
 function add3Points() {
     myPoints += 3;
 }
 
-add3Points();
-add3Points();
-add3Points();
-
 function remove1Point (){
     myPoints -= 1;
 }
 
+add3Points();
+add3Points();
+add3Points();
 remove1Point();
 remove1Point();
 
