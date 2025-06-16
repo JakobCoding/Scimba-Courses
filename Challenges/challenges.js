a34b0577c0eba4da0a9672edfe8fe6dec5f3601f
Jay Cee <jakechalmers86@gmail.com>
2025-06-16 19:41:17 +0800

    Uddating New Calculator challenge

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -77,13 +77,33 @@ console.log(myPoints);
 * When the user clicks the purchase button, render out
 * "Something went wrong, please try again" in the paragraph
 * that has the id="error".
-*/
+
 
 let showText = document.getElementById("text-content");
     console.log(showText)
 
 function howToRender() {
     console.log("button clicked!")
-    showText.textContent = "create a button with an id, store the id in variable let showText = document.getElementByID('id'), create function howToShowText(){}, Inside function type - showText.textContent = 'Hooray you rendered text!'";
-   
+    showText.textContent = "create a button with an id, store the id in variable let showText = document.getElementByID('id'), create function howToShowText(){}, Inside function type - showText.textContent = 'Hooray you rendered text!'"; 
 }
+
+* CALCULATOR CHALLENGE
+
+* Create four functions: add(), subtract(), divide(), multiply()
+* Call the correct function when the user clicks on one of the buttons
+* Perform the given calculation using num1 and num2
+* Render the result of the calculation in the paragraph with id="sum-el"
+
+* E.g. if the user clicks on the "Plus" button, you should render
+* "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+*/ 
+
+let num1 = 8;
+let num2 = 2;
+
+document.getElementById("num1-el")//.textContent = num1;
+document.getElementById("num2-el").textContent = num2;
+
+
+console.log("numb1-el");
