4f0ec1f8e8a023f9d5fad31db8a2a8ed46bbb5d0
Jay Cee <jakechalmers86@gmail.com>
2025-06-16 19:43:27 +0800

    Added basic Calculator funtions

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -102,8 +102,16 @@ function howToRender() {
 let num1 = 8;
 let num2 = 2;
 
-document.getElementById("num1-el")//.textContent = num1;
+document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
+function add() {}
 
-console.log("numb1-el");
+function subtract() {}
+
+function divide () {}
+
+function multiply() {}
+
+
+
