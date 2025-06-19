619696f0ae9c378df3736bcc9f2bd3810fcddc9e
Jake <jakechalmers86@gmail.com>
2025-06-19 17:10:10 +0800

    Generated random number between 2 & 11 and stored it in firstCard variable

---
+++ BlackJackGame/index.js
@@ -0,0 +1,12 @@
+/*
+
+* Create two variables, firstCard and secondCard. 
+
+* Set their values to a random number between 2-11
+
+* Create a variable, sum, and set it to the sum of the two cards
+
+*/
+
+let firstCard = Math.floor(Math.random() * 10) + 2;
+console.log(firstCard)
