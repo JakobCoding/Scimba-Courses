57a89b424e94260f747b3bd2094c655f793a5767
Jake <jakechalmers86@gmail.com>
2025-06-19 17:21:15 +0800

    Coding BlackJack logic

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -6,12 +6,21 @@
 
 * Create a variable, sum, and set it to the sum of the two cards
 
-*/
+
+* How to Generate a random number between 2 & 11
 
 let firstCard = Math.floor(Math.random() * 10) + 2;
 console.log(firstCard)
 
 let secondCard = Math.floor(Math.random() * 10) + 2;
 console.log(firstCard)
+*/
 
+
+let firstCard = 6;
+let secondCard = 9;
 let sum = firstCard + secondCard;
+
+if (sum === 21){
+    console.log("BlackJack you Win!")
+}
