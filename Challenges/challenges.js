52ecf1de93295b34dc95863eb1e236859276c3d5
Jake <jakechalmers86@gmail.com>
2025-06-19 16:20:20 +0800

    Completed Add & Subtract functions

--- Challenges/challenges.js
+++ Challenges/challenges.js
@@ -105,9 +105,21 @@ let num2 = 2;
 document.getElementById("num1-el").textContent = num1;
 document.getElementById("num2-el").textContent = num2;
 
-function add() {}
+let sumEl = document.getElementById("sum-el");
 
-function subtract() {}
+function add() {
+    let result = num1 + num2;
+    sumEl.textContent = "Sum: " + result;
+   
+}
+
+let minusEl = document.getElementById("sum-el");
+
+function subtract() {
+    result = num1 - num2;
+    minusEl.textContent = "Sum: " + result;
+
+}
 
 function divide () {}
 
